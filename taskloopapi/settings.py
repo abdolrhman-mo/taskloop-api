8ba6d69b76412f85db7ec65b425c3b6910cec9a6
abdolrhman-mo <abdelrhmanlearn@gmail.com>
2025-04-30 15:43:13 +0300

    Commited

--- taskloopapi/settings.py
+++ taskloopapi/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-aht1#!sr^+%s3jouk64!ps1cp13q_zlus&25j+*c#%g759xf!2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

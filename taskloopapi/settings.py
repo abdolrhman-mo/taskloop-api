7bc5ebc7424d93622ad773e3f19feb1d586d805f
abdolrhman-mo <abdelrhmanlearn@gmail.com>
2025-05-14 14:33:17 +0300

    runned python manage.py collectt static --noinput

--- taskloopapi/settings.py
+++ taskloopapi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-aht1#!sr^+%s3jouk64!ps1cp13q_zlus&25j+*c#%g759xf!2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -141,6 +141,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
